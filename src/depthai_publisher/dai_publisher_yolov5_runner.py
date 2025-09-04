8121ac7c2caacbf62b8a8baa5cc5a947e901ecc9
arycunningham <n10791442@qut.edu.au>
2025-09-04 22:55:28 +1000

    added v1_1_2

--- src/depthai_publisher/dai_publisher_yolov5_runner.py
+++ src/depthai_publisher/dai_publisher_yolov5_runner.py
@@ -30,9 +30,9 @@ syncNN = True
 # model path
 modelsPath = "/home/cdrone/catkin_ws/src/depthai_publisher/src/depthai_publisher/models"
 # modelName = 'exp31Yolov5_ov21.4_6sh'
-modelName = 'v1'
+modelName = 'v1_1_2'
 # confJson = 'exp31Yolov5.json'
-confJson = 'v1.json'
+confJson = 'v1_1_2.json'
 
 ################################  Yolo Config File
 # parse config
