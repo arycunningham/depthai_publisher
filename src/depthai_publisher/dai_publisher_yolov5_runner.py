6a04527e817235edd2833c37168aa7b9c23cdefd
arycunningham <ary.cunningham1@gmail.com>
2025-10-14 10:03:30 +1000

    changed pose source to mavros

--- src/depthai_publisher/dai_publisher_yolov5_runner.py
+++ src/depthai_publisher/dai_publisher_yolov5_runner.py
@@ -118,10 +118,10 @@ class DepthaiCamera():
         self.pub_marker_array = rospy.Publisher(self.pub_topic_marker_array, MarkerArray, queue_size=10, latch=True)
 
         ## Subscribe to UAV pose from MAVROS
-        # self.sub_uav_pose = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.callback_uav_pose)
+        self.sub_uav_pose = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.callback_uav_pose)
 
         ## Subscribe to UAV Emulated pose
-        self.sub_uav_pose = rospy.Subscriber('/uavasr/pose', PoseStamped, self.callback_uav_pose)
+        # self.sub_uav_pose = rospy.Subscriber('/uavasr/pose', PoseStamped, self.callback_uav_pose)
 
         ## UAV pose storage
         self.current_uav_pose = None
